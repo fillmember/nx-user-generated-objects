d792b0bdf51cd132d60fceb554d93562ad387f09
Ten <ten@mutate.space>
2021-01-27 06:43:16 +0100

    feat: fn - truncate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,9 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const { db = {} } = functions.config()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +12,24 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// export const matrices = functions.https.onRequest((req,res) => { })
+// export const list = functions.https.onRequest((req,res) => {})
+// export const create = functions.https.onRequest((req,res) => {})
+
+const maximumObjectCount = db.maxRecordCount || 2000
+export const truncate = functions.database.ref('/objects').onWrite((change) => {
+  const parentRef = change.after.ref;
+  const snapshot = change.after
+  if (snapshot.numChildren() >= maximumObjectCount) {
+    let i = 0;
+    const updates: Record<string, null> = {}
+    snapshot.forEach(child => {
+      if (++i <= snapshot.numChildren() - maximumObjectCount) {
+        updates[child.key] = null
+      }
+    })
+    return parentRef.update(updates)
+  }
+  return null
+})

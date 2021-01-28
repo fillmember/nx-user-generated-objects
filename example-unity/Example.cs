80cc2459f62be191ec8783919ab76f988025e614
Ten <ten@mutate.space>
2021-01-28 20:33:27 +0100

    add placeholder for unity example

---
+++ example-unity/Example.cs
@@ -0,0 +1,16 @@
+namespace NAXS.UserGeneratedContent
+{
+  using Firebase;
+  using Firebase.Database;
+  using Firebase.Extensions;
+  using System;
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+
+  public class Manager
+  {
+    AppOptions appOptions;
+  }
+
+}
